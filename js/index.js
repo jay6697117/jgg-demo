af240b68578ea542404dcd8d74bf5bf906dbecb0
stevezhang <871506263@qq.com>
2019-05-18 14:59:00 +0800

    九宫格成功版

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 !(function() {
-  function run() {
+  function run(colParam) {
     //设置图片最大数量
     let imgNum = 30;
+
     // 获取需要的标签
     let boxWidth, imgWidth, imgHeight, boxHeight;
     let btns = document.getElementById('top').children; //全部按钮
     let boxs = document.getElementById('bottom').children; //全部box盒子
+
     // 定义变量
     let containerWidth = (718 / 750) * document.documentElement.clientWidth;
     let boxPadding = (2 / 750) * document.documentElement.clientWidth;
+
     //创造两个数组
     let arr1 = [];
     for (let index = 0; index < imgNum; index++) {
@@ -20,42 +23,71 @@
       const elem = `孙怡邓伦牵手演绎刺绣奇缘${index + 1}`;
       arr2.push(elem);
     }
+
     //载入图片和文字节点的开关
     loadNodes(imgNum);
+
+    //获取盒子里面p1和p2的高度
     let p1Height = parseFloat(
       window.getComputedStyle(document.querySelectorAll('#bottom > .box > p:nth-of-type(1)')[imgNum - 1]).height
     ); //在函数外申明为了方便调用 建议遍历所有的盒子里面的p1取他们高度的最大值
     let p2Height = parseFloat(
       window.getComputedStyle(document.querySelectorAll('#bottom > .box > p:nth-of-type(2)')[imgNum - 1]).height
     ); //在函数外申明为了方便调用 建议遍历所有的盒子里面的p2取他们高度的最大值
-    //初始化三列
-    jgg(3);
+
+    //第一次执行分割为colParam列
+    jgg(colParam);
+
+    //重新获取盒子里面p1和p2的高度再次赋值给存储变量
+    p1Height = parseFloat(
+      window.getComputedStyle(document.querySelectorAll('#bottom > .box > p:nth-of-type(1)')[imgNum - 1]).height
+    ); //获取了最后一个盒子里面p1的高度(最后一个一定是最长的，同时也是高度最高的)
+    p2Height = parseFloat(
+      window.getComputedStyle(document.querySelectorAll('#bottom > .box > p:nth-of-type(2)')[imgNum - 1]).height
+    ); //获取了最后一个盒子里面p2的高度(最后一个一定是最长的，同时也是高度最高的)
+
+    //第二次执行分割为colParam列
+    jgg(colParam);
+
     // 遍历所有按钮监听点击事件
     for (let j = 2; j < btns.length + 2; j++) {
       const btn = btns[j - 2];
       btn.onclick = function() {
+        //第一次执行分割为j列
         jgg(j);
+
+        //重新获取盒子里面p1和p2的高度再次赋值给存储变量
         p1Height = parseFloat(
           window.getComputedStyle(document.querySelectorAll('#bottom > .box > p:nth-of-type(1)')[imgNum - 1]).height
         ); //获取了最后一个盒子里面p1的高度(最后一个一定是最长的，同时也是高度最高的)
         p2Height = parseFloat(
           window.getComputedStyle(document.querySelectorAll('#bottom > .box > p:nth-of-type(2)')[imgNum - 1]).height
         ); //获取了最后一个盒子里面p2的高度(最后一个一定是最长的，同时也是高度最高的)
+
+        //第二次执行分割为j列
         jgg(j);
+
+        // 监听手机旋转的事件的时机，重新设置
+        window.addEventListener('orientationchange', function() {
+          run(j);
+        });
+
+        // 监听手机窗口变化，重新设置
+        window.addEventListener('resize', function() {
+          run(j);
+        });
       };
     }
+
     function jgg(cols) {
       //750像素宽度下
       boxWidth = containerWidth / cols; //750像素设备宽度下的值
       imgWidth = boxWidth - boxPadding * 2; //750像素设备宽度下的值
       imgHeight = (308 / 220) * imgWidth; //750像素设备宽度下的值
       boxHeight = imgHeight + p1Height + p2Height + boxPadding * 2; //750像素设备宽度下的值
-      console.log(boxWidth);
-      console.log(boxHeight);
-      console.log(p1Height);
-      console.log(p2Height);
-      console.log(imgWidth);
-      console.log(imgHeight);
+
+      console.log('p1高度 :', p1Height);
+      console.log('p2高度 :', p2Height);
       //遍历boxs
       for (let i = 0; i < boxs.length; i++) {
         // 2.2.1 求出当前盒子所在的行和列
@@ -74,6 +106,7 @@
         box.style.top = rowNum * boxHeight + 'px';
       }
     }
+
     // 加载节点
     function loadNodes(num) {
       for (let i = 0; i < num; i++) {
@@ -86,10 +119,16 @@
     }
   }
 
-  // 首次加载应用，设置一次
-  run();
-  // 监听手机旋转的事件的时机，重新设置
-  window.addEventListener('orientationchange', run);
-  // 监听手机窗口变化，重新设置
-  window.addEventListener('resize', run);
+  // 首次加载应用运行
+  run(2);
+
+  // 首次加载应用运行，监听手机旋转的事件的时机，重新设置
+  window.addEventListener('orientationchange', function() {
+    run(2);
+  });
+
+  // 首次加载应用运行，监听手机窗口变化，重新设置
+  window.addEventListener('resize', function() {
+    run(2);
+  });
 })();
